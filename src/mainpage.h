e8a84dc3f7943bf443a43b9aea9d73b0552945a3
lsrea <>
2004-02-23 21:38:57 +0000

    First import of calibGenTKR package

---
+++ src/mainpage.h
@@ -0,0 +1,20 @@
+/** @mainpage package calibGenTKR
+  @author Leon Rochester
+
+  @section intro Introduction
+  This package is set up to run the root macro doBadStripsCalib as a compiled
+  program
+
+  ToDo:
+  <br>Modify to use a parameter file instead of explicit code
+  <br>Use TChain for input, even if only one file
+
+  <hr>
+  @section notes release notes
+  release.notes
+  <hr>
+  @section requirements requirements
+  @verbinclude requirements
+
+*/
+
