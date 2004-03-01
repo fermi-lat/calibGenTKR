2d8d82a194c10c73eddaa33310252f5ba612cac2
lsrea <>
2004-03-01 23:18:25 +0000

    more doc

--- src/mainpage.h
+++ src/mainpage.h
@@ -5,15 +5,43 @@
   This package is set up to run the root macro doBadStripsCalib as a compiled
   program.
 
-  There are several running modes. The current setting is simple cuts with no
-  count check. The other modes need a bit of work.
+  @section operation Operation
 
-  <hr>
-  @section notes release notes
-  release.notes
-  <hr>
-  @section requirements requirements
-  @verbinclude requirements
+  A large number of events are read in from one or more digi.root files. The events should
+  be reasonably populated with real tracks. Otherwise, the process becomes sensitive to 
+  the presence of noise hits, and the results will start to depend on the details of the 
+  threshold settings. In principle, each strip should be struck by some minimum number of tracks,
+  perhaps 40. For a single tower, with cosmic trigger, this implies about 50K events. 
+  The hit strips are accumulated in histograms, one for each plane.
+
+  The histograms are analyzed in one of several ways, only the first of which is official:
+
+  <ul> 
+  <li>Simple: the strips with zero hits are flagged as dead; those with occupancy greater than 
+  <em>maxOccupancy</em> above the ambient (default is 1%)
+  are flagged as hot. For events in a full-sized tower, this should be fine.</li>
+
+  <li>Standard: (This option needs some work.) The number of hits for each 
+  strip is tested against the expected value,
+  based on the average number of hits/strip for that plane. Strips with counts more than 
+  <em>nSigma</em> (default
+  is 6) sigma
+  below expectation, or zero counts, are flagged as dead; those with occupancy greater than
+  <em>hotFactor</em> (default is 10) times the ambient are flagged as hot.</li>
+
+  <li>Fit to local average: (This one needs even more work!) Like the previous, 
+  but the average used is the value of a polynomial fit
+  to the data in the histogram. This is supposed to compensate for effects in the EM, where
+  there are many fewer counts at the edges of the distributions.</li>
+  </ul>
+
+  A further test can be made on the dead strips, as to whether the expected value, based on the 
+  appropriate average, would have been high enough to preclude accidental zeroes. 
+  (This test is turned off by default.)
+
+  The job produces two xml files, one containing the dead strips, 
+  and another containing the hot strips.
+  In addition, the histograms are output in a root file.
 
   @section input Input parameters
 
@@ -39,4 +67,13 @@
   @param numEvents
   Maximum number of events to be processed. The actual number is the 
   minimum of this number and the number of events in the TChain.
+
+  @section notes release notes
+  release.notes
+  <hr>
+  @section requirements requirements
+  @verbinclude requirements
+
+
+
 */
