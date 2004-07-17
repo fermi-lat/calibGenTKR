83ec909bfe504c7ad8080efbe8ef657f80a0ab8e
lsrea <>
2004-07-17 15:28:00 +0000

    set detectorType from options file updated documentation

--- src/test/generateBadStrips.cxx
+++ src/test/generateBadStrips.cxx
@@ -3,45 +3,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "xml/IFile.h"
 
-using namespace std;
-
-/* to do:
-  figure out better local average, for now we use simple average
-  pass string for output files
-  input params from text file?
-  */
-
-/** @file generateBadStrips
+/** @file generateBadStrips.cxx
 @brief Driver for bad strips calibration
 */
 
-string stripBlanks(string &str) {
-    string temp = str;
-    if (temp.size()==0) return temp;
-    string::size_type pos;
-    pos = temp.find_first_not_of(" ", 0);
-    temp = temp.substr(pos);
-    pos = temp.find_last_not_of(" ", string::npos);
-    temp = temp.substr(0,pos+1);
-    return temp;
-}
-
-int splitString(string &input, string &LH, string &RH, char* delim) {
-    string::size_type pos;
-    pos = input.find(delim);
-    if (pos!=string::npos) {
-        LH = input.substr(0,pos);
-        RH = input.substr(pos);
-    } else {
-        LH = input;
-        RH = "";
-    }
-    LH = stripBlanks(LH);
-    RH = stripBlanks(RH);
-    return pos;
-}
 
 int main(int argn, char** argc) {
     
@@ -120,10 +86,13 @@ int main(int argn, char** argc) {
 
     cout << "Maximum number of events to process: " << numEvents << endl;
 
-    BadStripsCalib* r = 
-        new BadStripsCalib(digiChain, 0, 0, 
+    BadStripsCalib* r = new BadStripsCalib(
+        myFile,
+        digiChain, 0, 0, 
         const_cast<char*>(outputPrefix.c_str()),
-        const_cast<char*>(xmlPath.c_str()), const_cast<char*>(histPath.c_str()));  
+        const_cast<char*>(xmlPath.c_str()), 
+        const_cast<char*>(histPath.c_str())
+    );  
     r->Go(numEvents);
     r->Finish();
     r->WriteHist();
