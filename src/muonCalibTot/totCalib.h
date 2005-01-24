e737915626690c5597f9404acfc2a1897fc11eff
htajima <>
2005-01-24 06:57:42 +0000

    include modification by Takuya on xml file output and Gaussian convolved Landau function fit.

--- src/muonCalibTot/totCalib.h
+++ src/muonCalibTot/totCalib.h
@@ -6,7 +6,9 @@
 #include "TROOT.h"
 #include "TGraphErrors.h"
 #include "TChain.h"
+#include "TStyle.h"
 #include "TH1F.h"
+#include "TF1.h"
 #include "TProfile.h"
 #include "TFile.h"
 #include "TTree.h"
@@ -22,11 +24,14 @@ class totCalib {
   totCalib();
   ~totCalib();
 
-  void genTot(const char* digi, const char* recon, 
-		     const char* outputTxt, const char* outputRoot);
-  void genTot(TChain* digi, TChain* recon, 
-		     const char* outputTxt, const char* outputRoot);
+  int setInputRootFiles( const char* digi, const char* recon );
+  int setInputRootFiles( TChain* digi, TChain* recon );
+
+  bool setOutputFiles( const char* txt, const char* xml, const char* root);
+
   bool readTotConvFile(const char* dir, const char* runid);
+
+  void calibChargeScale( int );
 
  private:
 
@@ -42,6 +47,8 @@ class totCalib {
 
   void retrieveCluster();
 
+  void fillXml();//takuya
+
   int findTot(int planeId, TkrCluster::view viewId, int stripId);
 
   bool readTotConv(int layer, int view, const char* file);
@@ -49,21 +56,23 @@ class totCalib {
   float calcCharge(int planeId, TkrCluster::view viewId, int iStrip, 
 		   int tot) const;
 
-  static const int g_nLayer = 4;
+  static const int g_nLayer = 18;
   // g_nPlane=0 refers to top biLayer while g_nLayer=0 refers to bottom biLayer
-  static const int g_nPlane = 4;
+  static const int g_nPlane = 18;
   static const int g_nFecd = 24;
   static const int g_nView = 2;
   static const int g_nStrip = 1536;
 
   // group of strips so that each group has enough statistics
-  static const int g_nDiv = 64;
+  static const int g_nDiv = 24; //used to be 64;takuya
 
   TGraphErrors* m_totStrip[g_nPlane][g_nView];
   TGraphErrors* m_chargeStrip[g_nPlane][g_nView];
 
   TH1F* m_totHist[g_nPlane][g_nView][g_nDiv];
   TH1F* m_chargeHist[g_nPlane][g_nView][g_nDiv];
+
+  float m_chargeScale[g_nPlane][g_nView][g_nDiv];
 
   TNtuple* m_tuple;
 
@@ -88,11 +97,12 @@ class totCalib {
  
   //name of ascii output tot file
   std::string m_txtOutput;
+  std::string m_xmlOutput;//takuya
 
   float m_totQuadra[g_nLayer][g_nView][g_nStrip];
   float m_totGain[g_nLayer][g_nView][g_nStrip];
   float m_totOffset[g_nLayer][g_nView][g_nStrip];
 
 };
-
+Double_t langaufun(Double_t *x, Double_t *par);//takuya0122
 #endif
